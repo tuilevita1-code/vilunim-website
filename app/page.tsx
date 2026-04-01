924f7ea008117916866bdfdbf55911d89e3bef38
tuilevita1 <tuilevita1@gmail.com>
2026-04-01 21:57:45 +1300

    remove testimonials section

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,6 @@ import Hero from "./components/Hero";
 import Problem from "./components/Problem";
 import Solution from "./components/Solution";
 import HowItWorks from "./components/HowItWorks";
-import SocialProof from "./components/SocialProof";
 import FAQ from "./components/FAQ";
 import FinalCTA from "./components/FinalCTA";
 import Footer from "./components/Footer";
@@ -17,7 +16,6 @@ export default function Home() {
         <Problem />
         <Solution />
         <HowItWorks />
-        <SocialProof />
         <FAQ />
         <FinalCTA />
       </main>
